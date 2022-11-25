83a92038d60745f5c24c89d77f306b0ddb65464a
Oleg Lukashov <porshina1992@gmail.com>
2022-11-25 15:04:07 +0300

    2

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -4,3 +4,12 @@
 const myArray = [["John", 23], ["cat", 2]];
 myArray.push(["dog", 3]);
 // Only change code below this line
+
+-------------------------------------------
+
+//manipulate-arrays-with-pop
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop()
+// Only change code below this line
