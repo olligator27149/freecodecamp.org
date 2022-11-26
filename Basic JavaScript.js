5aa2ba05009d01ce0f91f62308c8e5d9c6a27a0a
Oleg Lukashov <porshina1992@gmail.com>
2022-11-26 18:04:27 +0300

    5

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -47,4 +47,15 @@ const myList = [["Chocolate Bar", 15],
 ["Chocolate Bar", 15]
 ];
 
+-------------------------------------------
+
+//write-reusable-javascript-with-functions
+
+function reusableFunction() {
+    console.log("Hi World");
+ }
+ reusableFunction()
+
+ 
+
 
