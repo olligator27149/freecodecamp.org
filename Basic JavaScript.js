e1b270858a75fa4cdd33e88049c8b6ba48deb70d
Oleg Lukashov <porshina1992@gmail.com>
2022-11-28 09:46:58 +0300

    10

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -130,6 +130,23 @@ function myOutfit() {
 
 myOutfit();
 
+---------------------------------------
 
+//understanding-undefined-value-returned-from-a-function
 
+// Setup
+let sum = 0;
 
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum = sum + 5;
+}
+// Only change code above this line
+
+addThree();
+addFive(0);
