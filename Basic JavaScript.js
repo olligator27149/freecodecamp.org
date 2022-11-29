30bf114801ada6206010a03176b6a4b376129733
Oleg Lukashov <porshina1992@gmail.com>
2022-11-29 15:18:15 +0300

    18

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -251,3 +251,16 @@ function testNotEqual(val) {
   }
   
   testNotEqual(10);
+
+  ---------------------------------------
+  //comparison-with-the-strict-inequality-operator
+
+// Setup
+function testStrictNotEqual(val) {
+    if (val!==17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
