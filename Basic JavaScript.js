8a6c0026592a6a234996e0de9de4fea90cc74add
Oleg Lukashov <porshina1992@gmail.com>
2022-11-29 12:24:11 +0300

    14

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -200,4 +200,18 @@ function trueOrFalse(wasThatTrue) {
   
   }
 
+  ---------------------------------------
+
+  //comparison-with-the-equality-operator
+
+  // Setup
+function testEqual(val) {
+    if (val==12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testEqual(10);
+
   
