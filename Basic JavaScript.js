698439b75da540ff1a8dfb3726e8514ef68931f8
Oleg Lukashov <porshina1992@gmail.com>
2022-11-30 07:46:31 +0300

    19

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -264,3 +264,19 @@ function testStrictNotEqual(val) {
   }
   
   testStrictNotEqual(10);
+
+  ---------------------------------------
+  //comparison-with-the-greater-than-operator
+  function testGreaterThan(val) {
+    if (val>100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val>10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  testGreaterThan(10);
