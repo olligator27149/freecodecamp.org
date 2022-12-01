7262999bb4e9f8d774803f7f5ef91c8dfe5ff069
Oleg Lukashov <porshina1992@gmail.com>
2022-12-01 15:27:16 +0300

    23

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -328,3 +328,19 @@ function testGreaterOrEqual(val) {
   }
   
   testLessOrEqual(10);
+
+  ---------------------------------------
+  //comparisons-with-the-logical-and-operator
+
+  function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val>=25&&val<=50) {
+        return "Yes";
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(10);
