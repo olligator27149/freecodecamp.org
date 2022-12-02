a85131c50eb05141b378eab5a924e66a6acd89ec
Oleg Lukashov <porshina1992@gmail.com>
2022-12-02 14:46:30 +0300

    25

--- codewarsSQL.js
+++ codewarsSQL.js
@@ -10,5 +10,6 @@ SELECT mod (number1, number2) FROM decimals
 
 --------------------------------------------
 
-
+//This code does not execute properly. Try to figure out why.
+SELECT price * amount AS total FROM items
 
