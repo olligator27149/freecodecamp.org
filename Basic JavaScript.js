a74da98ed30fe8394fcbdbe82ea8e646bf47bd66
Oleg Lukashov <porshina1992@gmail.com>
2022-12-02 15:37:46 +0300

    28

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -394,3 +394,18 @@ function testGreaterOrEqual(val) {
   testElseIf(7);
 
   ---------------------------------------
+
+//logical-order-in-if-else-statements
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+---------------------------------------
