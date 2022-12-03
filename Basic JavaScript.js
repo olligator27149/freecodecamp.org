bc9120d04e678a5ec777fea29fd6012c86ef8656
Oleg Lukashov <porshina1992@gmail.com>
2022-12-03 10:24:05 +0300

    32

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -512,4 +512,34 @@ switch (val) {
 caseInSwitch(1);
 
 ---------------------------------------
+//adding-a-default-option-in-switch-statements
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case "a":
+    answer = "apple";
+    break;
+  case "b":
+    answer = "bird";
+    break;
+  case "c":
+    answer = "cat";
+    break;
+  default:
+    answer = "stuff";
+    break;
+}
+
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+---------------------------------------
+
+
 
