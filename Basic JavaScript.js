4842d938656802d592ffe565d326661cca272a6c
Oleg Lukashov <porshina1992@gmail.com>
2022-12-03 10:26:31 +0300

    35

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -599,3 +599,14 @@ switch (val) {
 chainToSwitch(7);
 
 ---------------------------------------
+//returning-boolean-values-from-functions
+function isLess(a, b) {
+  // Only change code below this line
+    return (a < b);
+
+  // Only change code above this line
+}
+
+isLess(10, 15);
+
+---------------------------------------
