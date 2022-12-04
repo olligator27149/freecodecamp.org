35a541ad44fb3a42fe47b09097840269ab8228a8
Oleg Lukashov <porshina1992@gmail.com>
2022-12-04 21:01:18 +0300

    39

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -677,4 +677,17 @@ const myDog = {
 };
 
 ---------------------------------------
+//accessing-object-properties-with-dot-notation
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+---------------------------------------
 
