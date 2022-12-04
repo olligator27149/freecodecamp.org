66a75a97012b509203914c1ed32e978055e489a4
Oleg Lukashov <porshina1992@gmail.com>
2022-12-04 20:58:50 +0300

    37

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -628,3 +628,39 @@ if (a < 0) {
 abTest(2,2);
 
 ---------------------------------------
+//counting-cards
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+switch (card){
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count++;
+    break;
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+    count--;
+    break;
+}
+var hren = 'Hold';
+
+if (count > 0) {
+  hren = 'Bet';
+}
+  
+return count + " " + hren;
+
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+---------------------------------------
