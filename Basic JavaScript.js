998d8386a4d0872cc7e2b13380ab18995dca7608
Oleg Lukashov <porshina1992@gmail.com>
2022-12-06 14:56:57 +0300

    47

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -807,5 +807,21 @@ const myMusic = [
 ];
 
 ---------------------------------------
+//accessing-nested-objects
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
 
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+---------------------------------------
 
