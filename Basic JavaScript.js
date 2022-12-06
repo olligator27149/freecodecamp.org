03901663d3e664eaa7d8a2f16de2d0d312abde61
Oleg Lukashov <porshina1992@gmail.com>
2022-12-06 14:58:10 +0300

    48

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -824,4 +824,27 @@ const myStorage = {
 const gloveBoxContents = myStorage.car.inside["glove box"];
 
 ---------------------------------------
+//accessing-nested-arrays
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+
+---------------------------------------
 
