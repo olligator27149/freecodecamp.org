a0303d5836f95b94c9c75de2c1bc3a0ea6b9e3c4
Oleg Lukashov <porshina1992@gmail.com>
2022-12-07 15:03:44 +0300

    55

--- codewars.js
+++ codewars.js
@@ -3,4 +3,13 @@
 function multiply(a, b){
     return a * b
   }
-  
+
+--------------------------------------------
+//Training JS #1: create your first JS function and print "Hello World!"
+//refer to the example and write your first JS function
+function helloWorld() {
+  var str = "Hello World!";
+  console.log(str);
+};
+
+--------------------------------------------
