8e86ce9c4ad24401217468f66ef9b9fdeebb3b8b
Oleg Lukashov <porshina1992@gmail.com>
2022-12-08 15:20:36 +0300

    59

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -965,3 +965,15 @@ do {
 } while (i < 5);
 
 ---------------------------------------
+//replace-loops-using-recursion
+function sum(arr, n) {
+  // Only change code below this line
+if (n <= 0) {
+      return 0;
+    } else {
+      return arr[n - 1] + sum(arr, n - 1);
+    }
+  // Only change code above this line
+}
+
+---------------------------------------
