274590ecef9d5bd2dfbf079d0adc4fe444d40abc
Oleg Lukashov <porshina1992@gmail.com>
2022-12-13 16:17:12 +0300

    80

--- ES6.js
+++ ES6.js
@@ -72,4 +72,11 @@ const sum = (...args) => {
     return args.reduce((a, b) => a + b, 0);
   }
 
-  
+  ---------------------------------------
+  //use-the-spread-operator-to-evaluate-arrays-in-place
+  const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
