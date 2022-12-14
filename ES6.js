9b4b6fab28be48f61fde3e7770a2a486ca88e702
Oleg Lukashov <porshina1992@gmail.com>
2022-12-14 11:12:19 +0300

    83

--- ES6.js
+++ ES6.js
@@ -112,3 +112,19 @@ const HIGH_TEMPERATURES = {
   // Only change code above this line
 
   ---------------------------------------
+  //use-destructuring-assignment-to-assign-variables-from-nested-objects
+  const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Only change code below this line
+    
+  //const lowToday = LOCAL_FORECAST.today.low;
+  //const highToday = LOCAL_FORECAST.today.high;
+  const {today: {low: lowToday, high: highToday }} = LOCAL_FORECAST;
+  // Only change code above this line
+
+  ---------------------------------------
+  
