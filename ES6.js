57129b8acea95945dab52db53635833bea959e7f
Oleg Lukashov <porshina1992@gmail.com>
2022-12-14 11:11:41 +0300

    82

--- ES6.js
+++ ES6.js
@@ -98,3 +98,17 @@ const HIGH_TEMPERATURES = {
   // Only change code above this line
 
   ---------------------------------------
+//use-destructuring-assignment-to-assign-variables-from-objects
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+    
+  
+  const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
+  // Only change code above this line
+
+  ---------------------------------------
