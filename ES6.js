522d93192e2783c6ec02f75f5b01b98c3245081e
Oleg Lukashov <porshina1992@gmail.com>
2022-12-14 11:13:37 +0300

    85

--- ES6.js
+++ ES6.js
@@ -136,3 +136,15 @@ const HIGH_TEMPERATURES = {
 console.log("a=" + a, "b=" + b);
 
 ---------------------------------------
+//use-destructuring-assignment-with-the-rest-parameter-to-reassign-array-elements
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [a, b, ...shorterList] = list; // Change this line
+    // Only change code above this line
+    return shorterList;
+  }
+  
+  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+  ---------------------------------------
