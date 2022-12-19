d9316cf8725ebc66bb672f59614320918578c538
Oleg Lukashov <porshina1992@gmail.com>
2022-12-19 14:39:37 +0300

    94

--- ES6.js
+++ ES6.js
@@ -195,3 +195,16 @@ testNotEqual(10);
   );
 
   ----------------------------------------
+//write-concise-declarative-functions-with-es6
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+----------------------------------------
