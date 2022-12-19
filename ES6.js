d196c33515f00f8a498aa71d0e7d0150820e9b75
Oleg Lukashov <porshina1992@gmail.com>
2022-12-19 14:38:24 +0300

    92

--- ES6.js
+++ ES6.js
@@ -174,22 +174,15 @@ function removeFirstTwo(list) {
   // Only change code above this line
 
   --------------------------------------- 
-  //create-strings-using-template-literals
-  const result = {
-    success: ["max-length", "no-amd", "prefer-arrow-functions"],
-    failure: ["no-var", "var-on-top", "linebreak"],
-    skipped: ["no-extra-semi", "no-dup-keys"]
-  };
-  function makeList(arr) {
-    // Only change code below this line
-    const failureItems = [];
-      for (let i = 0; i < arr.length; i++) {
-      failureItems.push(`<li class="text-warning">${arr[i]}</li>`);}
-    // Only change code above this line
-  
-    return failureItems;
+  //comparison-with-the-inequality-operator
+// Setup
+function testNotEqual(val) {
+  if (val!=99) { // Change this line
+    return "Not Equal";
   }
-  
-  const failuresList = makeList(result.failure);
+  return "Equal";
+}
+
+testNotEqual(10);
 
   ------------------------------------------------------
