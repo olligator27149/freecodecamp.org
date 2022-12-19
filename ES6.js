a0d13559cff52462f7b47013f6cbb10166b36a1a
Oleg Lukashov <porshina1992@gmail.com>
2022-12-19 14:39:04 +0300

    93

--- ES6.js
+++ ES6.js
@@ -185,4 +185,13 @@ function testNotEqual(val) {
 
 testNotEqual(10);
 
-  ------------------------------------------------------
+  ----------------------------------------
+  //write-concise-object-literal-declarations-using-object-property-shorthand
+  const createPerson = (name, age, gender) => (
+    // Only change code below this line
+   {name, age, gender}
+   
+    // Only change code above this line
+  );
+
+  ----------------------------------------
