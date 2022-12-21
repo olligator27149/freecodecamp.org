78d31e605414fbd3614057a664026ee94c09a9e9
Oleg Lukashov <porshina1992@gmail.com>
2022-12-21 16:25:25 +0300

    96

--- ES6.js
+++ ES6.js
@@ -221,3 +221,29 @@ const carrot = new Vegetable('carrot');
 console.log(carrot.name); // Should display 'carrot'
 
 ----------------------------------------
+//use-getters-and-setters-to-control-access-to-an-object
+// Only change code below this line
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+  
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
+
+
+
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+
+----------------------------------------
