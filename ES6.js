da444ff8b41ac14d0df02c94ed722eab4160544d
Oleg Lukashov <porshina1992@gmail.com>
2022-12-21 16:25:54 +0300

    97

--- ES6.js
+++ ES6.js
@@ -247,3 +247,14 @@ thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
 
 ----------------------------------------
+//create-a-module-script
+<html>
+  <body>
+    <!-- Only change code below this line -->
+<script type="module" src="index.js"></script>
+
+    <!-- Only change code above this line -->
+  </body>
+</html>
+
+----------------------------------------
