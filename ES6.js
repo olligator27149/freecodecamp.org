18ba7d763fd2db2bfe6e8540b07d033bb1768766
Oleg Lukashov <porshina1992@gmail.com>
2022-12-23 09:15:08 +0300

    106

--- ES6.js
+++ ES6.js
@@ -336,3 +336,20 @@ makeServerRequest.then(result => {
 });
 
 ----------------------------------------
+//handle-a-rejected-promise-with-catch
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.catch(error  => {
+  console.log(error);
+});
+
+----------------------------------------
