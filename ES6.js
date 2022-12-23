0bb49caab58b17ef6d3f16e546920975ed7f2534
Oleg Lukashov <porshina1992@gmail.com>
2022-12-23 09:13:45 +0300

    104

--- ES6.js
+++ ES6.js
@@ -307,3 +307,16 @@ const makeServerRequest = new Promise((resolve, reject) => {
 });
 
 ----------------------------------------
+//complete-a-promise-with-resolve-and-reject
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+----------------------------------------
