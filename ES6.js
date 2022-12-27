eccbf7b05a98099602e83f8cecf466a7e5055074
Oleg Lukashov <porshina1992@gmail.com>
2022-12-27 16:00:33 +0300

    109

--- ES6.js
+++ ES6.js
@@ -372,3 +372,9 @@ let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
 
 ----------------------------------------
+//match-literal-strings
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+
+----------------------------------------
