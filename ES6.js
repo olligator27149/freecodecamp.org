bf525b5ea54c7261789767359418cc23ca3f61c7
Oleg Lukashov <porshina1992@gmail.com>
2022-12-29 10:20:54 +0300

    112

--- ES6.js
+++ ES6.js
@@ -390,3 +390,9 @@ let fccRegex = /freeCodeCamp|FreeCodeCamp|FreeCOdeCamp|FreeCodecamp|FrEeCoDeCamp
 let result = fccRegex.test(myString);
 
 ----------------------------------------
+//extract-matches
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+
+----------------------------------------
